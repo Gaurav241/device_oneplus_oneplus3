a2a9309be320657974da02cc45131d4c1d86dadc
LuK1337 <priv.luk@gmail.com>
2020-06-16 14:46:21 +0200

    msm8996-common: bluetooth: Set BTIF_HF_WBS_PREFERRED to FALSE
    * On Android <= 9 BTIF_HF_WBS_PREFERRED defaulted to FALSE, and
      now that it defaults to TRUE it looks like it exposed that we
      don't have properly working WBS on MI8996 devices atm (yet?).
    
    Change-Id: I0f08b2da9ae0b3500f4c2c36905d809adcef0d8a

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -57,6 +57,9 @@ static inline const char* BtmGetDefaultName()
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 
+/* Prefer non-WBS SCO */
+#define BTIF_HF_WBS_PREFERRED FALSE
+
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
 #endif
